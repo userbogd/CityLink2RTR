ae479b8b6c4b312f8096183cb2a3c5eef1679b11
Bogdan Pilyugin <bogd@live.ru>
2022-06-14 16:27:01 +0200

    charset fixed on windows, on linux not

--- src/main/java/CityLink2RTR/CityLinkRTRMain.java
+++ src/main/java/CityLink2RTR/CityLinkRTRMain.java
@@ -1,64 +1,61 @@
 package CityLink2RTR;
 
 import java.net.SocketException;
-
 import data.Connection;
 import data.Packet;
 import data.PacketHandler;
 import data.UID;
 import java.util.Arrays;
+import java.nio.charset.Charset;
 
 import MonitorHTTPServer.*;
-
 import CityLink2RTR.ThreadedUDPClient;
-
 
 public class CityLinkRTRMain {
 	static ThreadedUDPServer UDPServ;
 	static MonitorHTTPServer HTTP;
+	public int Counter;
+
 	public static void main(String[] args) {
-		
+
 		UDPServ = new ThreadedUDPServer(60500);
 		UDPServ.receive(new PacketHandler() {
+			int Counter;
+
 			@Override
 			public void process(Packet packet) {
 				String data = new String(packet.getData());
-			    if(data.length() >=13)
-			    {
-			        byte[] byteArray;
-			        byte[] b;
-			        // convert the string to a byte array
-			        // using platform's default charset
-			        byteArray = data.getBytes();
-			        b = new byte[13];
-			        for (int k=0; k<1000; k+=13)
-			        {
-			        	for(int kk=0;kk<13;++kk)
-			        		b[kk] = byteArray[k+kk];
-			        
-			        if(b[0] != 0)
-			        	System.out.println(bytesToHex(b));
-			        }
-			    }
+				Counter = 0;
+				if (data.length() >= 13) {
+					byte[] byteArray;
+					byte[] b;
+					byteArray = data.getBytes(Charset.forName("Windows-1251"));
+					b = new byte[13];
+					for (int k = 0; k < 1000; k += 13) {
+						for (int kk = 0; kk < 13; ++kk)
+							b[kk] = byteArray[k + kk];
+						if (b[0] != 0) {
+							System.out.println(bytesToHex(b));
+							++Counter;
+						}
+					}
+				}
+				System.out.println("Packet size:" + Integer.toString(Counter));
 			}
 		});
-		
 		HTTP = new MonitorHTTPServer();
-	
-	}
-	
-	
-	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for (int j = 0; j < bytes.length; j++) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-	        hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-	    }
-	    return new String(hexChars);
 	}
 
-	
+	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
+	public static String bytesToHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+			hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
 
 }
