a5f2cf8f370edc9daec269d3f8bfd01e89019f1d
Bogdan <bogd@live.ru>
2022-06-15 21:10:19 +0200

    added ini file

--- src/main/java/CityLink2RTR/CityLinkRTRMain.java
+++ src/main/java/CityLink2RTR/CityLinkRTRMain.java
@@ -1,6 +1,12 @@
 package CityLink2RTR;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.prefs.Preferences;
+
+import org.ini4j.Ini;
+import org.ini4j.InvalidFileFormatException;
 
 import MonitorHTTPServer.*;
 import SerialListener.SerialListener;
@@ -8,19 +14,49 @@ import UDPConnections.Packet;
 import UDPConnections.PacketHandler;
 import UDPConnections.ThreadedUDPServer;
 
-
 public class CityLinkRTRMain {
 	static ThreadedUDPServer UDPServ;
 	static MonitorHTTPServer HTTP;
-	//static SerialListener Serial1; 
-	//static SerialListener Serial2;
+	// static SerialListener Serial1;
+	// static SerialListener Serial2;
 	public int Counter;
+	public static Preferences prefs;
+	public static final String FILENAME = "rtrconfig.ini";
 
 	public static void main(String[] args) {
 
+		String filename = (args.length > 0) ? args[0] : FILENAME;
+		File conf = new File(filename);
 
-		
-		
+		if (!conf.exists()) {
+			System.out.println("Conf file not found. Created default");
+			try {
+				conf.createNewFile(); // if file already exists will do nothing
+				Ini ini = new Ini(conf);
+				ini.put("block_name", "property_name", "value");
+				ini.put("block_name", "property_name_2", 45);
+				ini.put("block_name", "property_name_3", 50);
+				ini.put("block_name", "property_name_4", 55);
+				ini.store();
+			} catch (InvalidFileFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		try {
+			Ini ini = new Ini(conf);
+			System.out.println(ini.get("block_name", "property_name"));
+			System.out.println(ini.get("block_name", "property_name_2"));
+			System.out.println(ini.get("block_name", "property_name_3"));
+			System.out.println(ini.get("block_name", "property_name_4"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		UDPServ = new ThreadedUDPServer(60500);
 		UDPServ.receive(new PacketHandler() {
 			int Counter;
@@ -46,12 +82,12 @@ public class CityLinkRTRMain {
 				System.out.println("Packet size:" + Integer.toString(Counter));
 			}
 		});
-		
+
 		HTTP = new MonitorHTTPServer();
-		//Serial1 = new SerialListener("COM3", 115200);
+		// Serial1 = new SerialListener("COM3", 115200);
 		new Thread(new SerialListener("/dev/ttyUSB0", 115200)).start();
-		//Serial2  = new SerialListener("COM4", 115200);
-		new Thread( new SerialListener("/dev/ttyUSB1", 115200)).start();
+		// Serial2 = new SerialListener("COM4", 115200);
+		new Thread(new SerialListener("/dev/ttyUSB1", 115200)).start();
 	}
 
 	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
---
+++ src/main/java/CityLink2RTR/ConfigReader.java
@@ -0,0 +1,15 @@
+package CityLink2RTR;
+
+
+
+public class ConfigReader {
+
+	
+
+	public ConfigReader(String filename) {
+		super();
+		
+
+	}
+
+}
